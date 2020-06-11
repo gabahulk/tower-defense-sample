e87615fa21bd98dfa89d2ef6bce309bb210dde74
Gabriel Fazzio de Paula <gabrielfpaula@gmail.com>
2020-06-11 15:15:16 -0300

    :tada: First commit!

---
+++ Assets/GamePiecePlacementController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiecePlacementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject GamePiecePrefab;
+
+    private KeyCode newTower = KeyCode.A; // debug
+    private GameObject currentGamePiece;
+
+    private void Update()
+    {
+        HandleNewObjectEvent();
+
+        if (currentGamePiece != null)
+        {
+            MoveCurrentPlaceableObjectToMouse();
+            if (Input.GetMouseButtonDown(0))
+            {
+                CommitGamePiece();
+            }
+        }
+    }
+
+    private void CommitGamePiece()
+    {
+        currentGamePiece = null;
+    }
+
+    private void MoveCurrentPlaceableObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            if (hitInfo.collider.gameObject.CompareTag("PlaceableTerrain"))
+            {
+                currentGamePiece.transform.position = hitInfo.point;
+            }
+        }
+    }
+
+    private void HandleNewObjectEvent()
+    {
+        if (Input.GetKeyUp(newTower))
+        {
+            if (currentGamePiece == null)
+            {
+                currentGamePiece = Instantiate(GamePiecePrefab);
+            }
+            else
+            {
+                Destroy(currentGamePiece);
+            }
+
+        }
+    }
+}
---
+++ Assets/Source/SpawnBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SpawnBehaviour : MonoBehaviour
+{
+    public GameObject ObjectToSpawn;
+
+    void Update()
+    {
+        
+    }
+}
